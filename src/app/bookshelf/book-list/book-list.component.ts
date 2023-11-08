f483ec9af4d4ecbef695f1aaeeab62150a6edbd5
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-11-08 15:24:45 -0500

    styling

--- src/app/bookshelf/book-list/book-list.component.ts
+++ src/app/bookshelf/book-list/book-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Book } from './shared/book/book.model';
+import { Book } from 'src/app/shared/book/book.model';
 
 @Component({
   selector: 'app-book-list',
@@ -7,5 +7,24 @@ import { Book } from './shared/book/book.model';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent {
-
+  myBooks: Book[] = [
+    new Book(
+      'Book of Testing',
+      'Will Wilder',
+      'Mystery',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'On the Road',
+      'Jack Kerouac',
+      'Literary Fiction',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'Tom Sawyer',
+      'Mark Twain',
+      'Adventure',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    )
+  ];
 }
--- src/app/library/book-results/book-results.component.ts
+++ src/app/library/book-results/book-results.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/shared/book/book.model';
 
 @Component({
   selector: 'app-book-results',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-results.component.css']
 })
 export class BookResultsComponent {
-
+  allBooks: Book[] = [
+    new Book(
+      'Book of Testing',
+      'Will Wilder',
+      'Mystery',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'On the Road',
+      'Jack Kerouac',
+      'Literary Fiction',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'Tom Sawyer',
+      'Mark Twain',
+      'Adventure',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    )
+  ];
 }
