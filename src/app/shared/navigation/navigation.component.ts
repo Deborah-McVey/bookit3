f6c42553fd3a97c5b1d7cc4e80deb08c2d7fc985
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-11-08 17:38:27 -0500

    navcomp

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
 
+
+export class NavigationComponent implements OnInit {
+  @Output() currentPage = new EventEmitter<string>();
   collapsed: boolean = true;
   show: boolean = false;
-  
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectPage(page: string) {
+    this.currentPage.emit(page);
+  }
+
 }
