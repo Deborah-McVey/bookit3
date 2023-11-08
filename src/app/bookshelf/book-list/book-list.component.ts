10282447e58977b4613f046d3ada3d5ac1b9489e
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-11-08 11:54:01 -0500

    stuck on importing bookmodel

---
+++ src/app/bookshelf/book-list/book-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Book } from './shared/book/book.model';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent {
+
+}
